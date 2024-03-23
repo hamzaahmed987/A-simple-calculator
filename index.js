232643202be0099110b113cb5d279e300f84c351
Hamza Ahmed <gboy76304@gmail.com>
2024-03-23 17:47:43 +0500

    updated

---
+++ index.js
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+async function calculate() {
+    const answer = await inquirer.prompt([
+        {
+            message: "Enter First number",
+            type: "number",
+            name: "firstNumber"
+        },
+        { message: "Enter Second number",
+            type: "number",
+            name: "secondNumber"
+        },
+        {
+            message: "select one of the operator to perform operation",
+            type: "list",
+            name: "operator",
+            choices: ["+", "-", "/", "*", "**", "%"],
+        },
+    ]);
+    if (answer.operator === "+") {
+        console.log(answer.firstNumber + answer.secondNumber);
+    }
+    else if (answer.operator === "-") {
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+    else if (answer.operator === "/") {
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+    else if (answer.operator === "*") {
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+    else if (answer.operator === "**") {
+        console.log(answer.firstNumber ** answer.secondNumber);
+    }
+    else if (answer.operator === "%") {
+        console.log(answer.firstNumber % answer.secondNumber);
+    }
+    else {
+        console.log("Please select valid operator");
+    }
+}
+let confirm;
+do {
+    await calculate();
+    confirm = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "con",
+            message: "Do you want to resatart?",
+        },
+    ]);
+    console.log(confirm);
+} while (confirm.con);
--- index.ts
+++ index.ts
@@ -1,30 +1,57 @@
+#! /usr/bin/env node 
+
 import inquirer from "inquirer";
 async function calculate() {
     const answer = await inquirer.prompt([
-      { message: "Enter First number", type: "number", name: "firstNumber" },
-      { message: "Enter Second number", type: "number", name: "secondNumber" },
+      {
+         message: "Enter First number",
+          type: "number",
+          name: "firstNumber" 
+      },
+      { message: "Enter Second number", 
+        type: "number",
+        name: "secondNumber" 
+      },
       {
         message: "select one of the operator to perform operation",
         type: "list",
         name: "operator",
-        choices: ["+", "-", "/", "*"],
+        choices: ["+", "-", "/", "*","**","%"],
       },
-    ]);
+     ]);
   
     if (answer.operator === "+") {
       console.log(answer.firstNumber + answer.secondNumber);
-    } else if (answer.operator === "-") {
+    }  else if (answer.operator === "-") {
       console.log(answer.firstNumber - answer.secondNumber);
-    } else if (answer.operator === "/") {
+    }  else if (answer.operator === "/") {
       console.log(answer.firstNumber / answer.secondNumber);
-    } else if (answer.operator === "*") {
+    }  else if (answer.operator === "*") {
       console.log(answer.firstNumber * answer.secondNumber);
-    } else {
+    }  else if (answer.operator === "**") {
+      console.log(answer.firstNumber ** answer.secondNumber);
+    }  else if (answer.operator === "%") {
+      console.log(answer.firstNumber % answer.secondNumber);
+    } 
+    else {
       console.log("Please select valid operator");
     }
   }
   
-  calculate();
+let confirm;
+  do{
+    await calculate();
+   confirm = await inquirer.prompt([
+    {
+      type:"confirm",
+      name:"con",
+      message:"Do you want to resatart?",
+    },
+  ]);
+  console.log(confirm)
+}
+while(confirm.con)
+ 
   
 
 
